f8d4e01c91b003e00ccf124437d565876a148f72
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-04-02 13:49:41 +0100

    exercicio banco de dados

--- alunos.js
+++ alunos.js
@@ -1,51 +1,32 @@
 const express = require("express");
 const app = express();
-
+const db = require("./dataBase")
+const dbContext = db.AlunosDataBase()
 app.use(express.json());
-let data = [];
 
-app.get("/users", (req, res) => {
-    console.log("entrou no get")
-    res.status(201).send({alunos: data});
+
+
+app.get("/users", async (req, res) => {
+    res.status(201).send(await dbContext.list());
 })
 
-app.get("/users/:id", (req, res) => {
-    console.log("entrou no get by id")
-    let idParams = req.params.id
-    let index = data.findIndex((item) => item.id === idParams)
-
-    let newData = data[index]
-    res.status(201).send({alunos: newData})
+//BY ID
+app.get("/users/:id", async (req, res) => {
+    res.status(201).send(await dbContext.get(req.params.id))
 })
 
-app.post("/users", (req, res) => {
-    console.log("entrou no post")
-    let newBody = req.body
-    data.push(newBody)
-
-    res.status(201).send({alunos: data})
+app.post("/users", async (req, res) => {
+    res.status(201).send(await dbContext.insert(req.body))
 })
 
 
-app.put("/users/:id", (req,res) => {
-    console.log("entrando no put");
-    const alunoId = req.params.id;
-    let index = data.findIndex((item) => item.id === alunoId)
-    
-    data[index] = req.body
-
-    res.status(201).send({aluno: data[index] = req.body});
+app.put("/users/:id", async (req,res) => {
+    res.status(201).send(await dbContext.update(req.body, req.params.id));
 })
 
-app.delete("/users/:id", (req,res) => {
-    console.log("entrando no delete");
-    
-    const alunoId = req.params.id
-    let index = data.findIndex((aluno) => aluno.id === alunoId)
-
-    data.splice(index, 1)
-
-    res.status(201).send({alunos: data})
+app.delete("/users/:id", async (req,res) => {
+   
+    res.status(201).send(await dbContext.del(req.params.id))
 })
 
 app.listen(3001, () => {
